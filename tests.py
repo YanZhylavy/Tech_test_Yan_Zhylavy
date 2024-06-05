620fe819dfea672da7a7b9cf06582864ba6afb18
Yan Zhylavy <yanzhylavy@gmail.com>
2024-06-05 10:17:03 +0300

    Added unit tests for get_count() and count_to_file()

--- tests.py
+++ tests.py
@@ -7,14 +7,60 @@ from words_counter import WordCounter
 class TestWordCounter(unittest.TestCase):
 
     @unittest.mock.patch('builtins.print')
-    def test_stdout(self, print_mock):
+    def test_positive_stdout(self, print_mock):
         with tempfile.NamedTemporaryFile(delete=False, mode='w+', encoding='utf-8',delete_on_close=True) as file:
             content = 'Lorem ipsum dolor ipsum lorem'
             file.write(content)
             file.flush()
-            WordCounter.get_count_to_st_out(file.name)
+
+            word_counter = WordCounter(file.name)
+            word_counter.get_count_to_std_out()
             expected = [call('2 lorem'),call('2 ipsum'), call('1 dolor')]
-            print_mock.assert_has_calls(expected, any_order=False)   
+            
+            print_mock.assert_has_calls(expected, any_order=False) 
+
+
+    @unittest.mock.patch('builtins.print')
+    def test_negative_std_out_no_such_file(self, mock_print):
+        word_counter = WordCounter('nonexisting_file')
+        word_counter.get_count_to_std_out()
+        mock_print.assert_called_once_with(('You have to pass correct filename to WordCounter constructor. '\
+                                            'File have to be at the same directory, or use full path.'))
+        
+
+    def test_positive_get_count_as_dict(self):
+        with tempfile.NamedTemporaryFile(delete=False, mode='w+', encoding='utf-8',delete_on_close=True) as file:
+            content = 'Lorem ipsum dolor ipsum lorem'
+            file.write(content)
+            file.flush()
+
+            word_counter = WordCounter(file.name)
+            result = word_counter.get_count()
+            expected = {'lorem': 2, 'ipsum': 2, 'dolor': 1}
+
+            self.assertDictEqual(result, expected)
+
+
+    @unittest.mock.patch('builtins.open')
+    def test_positive_writing_to_file(self, mock_open_arg):
+        mock_input_file = unittest.mock.mock_open(read_data='Lorem lorem ipsum')
+        mock_output_file = unittest.mock.mock_open()
+
+        mock_open_arg.side_effect = [mock_input_file.return_value, mock_output_file.return_value]
+
+        input_file_path = 'input.txt'
+        output_file_path = 'output.txt'
+
+        word_counter = WordCounter(input_file_path)
+        word_counter.count_to_file(output_file_path)
+      
+        mock_open_arg.assert_any_call(input_file_path, 'r')
+        mock_open_arg.assert_any_call(output_file_path, 'w+')
+
+        mock_input_file().read.assert_called_once()
+
+        mock_output_file().write.assert_called_once_with('2 lorem\n1 ipsum')
+
 
 
 if __name__ == '__main__':
--- words_counter.py
+++ words_counter.py
@@ -1,17 +1,42 @@
 class WordCounter:
+    '''
+    Please pass the name of the file from which you want to read the text for word counting 
+    to the WordCount constructor when creating the object. 
+    It is recommended to use the full path to the file.
+    Use methods get_count(), get_count_to_std_out() and
+    count_to_file() to retrieve words count in different ways.
 
-    @staticmethod
-    def get_count_to_st_out(filename: str) -> None:
-        counter = {}
-        with open(filename, 'r') as f:
-            all_words_list = f.read().split()
-            for word in all_words_list:
-                word = word.lower()
-                if word not in counter:
-                    counter[word] = 1
-                else:
-                    counter[word] += 1 
+    '''
+    def __init__(self, filename: str):
+        self._counter = {}
+        try:
+            with open(filename, 'r') as f:
+                all_words_list = f.read().split()
+                for word in all_words_list:
+                    word = word.lower()
+                    if word not in self._counter:
+                        self._counter[word] = 1
+                    else:
+                        self._counter[word] += 1 
+        except FileNotFoundError:
+            print(('You have to pass correct filename to WordCounter constructor. ' \
+                   'File have to be at the same directory, or use full path.'))                
 
-        for words, counts in counter.items():
-            print(f'{counts} {words}')  
+
+    def get_count_to_std_out(self) -> None:
+
+        for word, count in self._counter.items():
+            print(f'{count} {word}')  
             
+
+    def get_count(self) -> dict:
+        return self._counter    
+
+
+    def count_to_file(self, output_file: str) -> None:
+        with open(output_file, 'w+') as f:
+            lines = [f'{count} {word}' for word, count in self._counter.items()]
+            lines = '\n'.join(lines)
+            f.write(lines)
+        
+                            
